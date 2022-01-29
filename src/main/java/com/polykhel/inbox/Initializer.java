35fa2e8254d96cf83960f843c52d18fedb4f4ae2
Michael Pollente <pollentem@gmail.com>
2022-01-30 00:23:46 +0800

    Add message view to inbox page

--- src/main/java/com/polykhel/inbox/Initializer.java
+++ src/main/java/com/polykhel/inbox/Initializer.java
@@ -1,18 +1,25 @@
 package com.polykhel.inbox;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import com.polykhel.inbox.emallist.EmailListItem;
+import com.polykhel.inbox.emallist.EmailListItemKey;
+import com.polykhel.inbox.emallist.EmailListItemRepository;
 import com.polykhel.inbox.folders.Folder;
 import com.polykhel.inbox.folders.FolderRepository;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Component
 public class Initializer {
 
     private final FolderRepository folderRepository;
+    private final EmailListItemRepository emailListItemRepository;
 
-    public Initializer(FolderRepository folderRepository) {
+    public Initializer(FolderRepository folderRepository, EmailListItemRepository emailListItemRepository) {
         this.folderRepository = folderRepository;
+        this.emailListItemRepository = emailListItemRepository;
     }
 
     @PostConstruct
@@ -20,5 +27,20 @@ public class Initializer {
         folderRepository.save(new Folder("polykhel", "Inbox", "blue"));
         folderRepository.save(new Folder("polykhel", "Sent", "green"));
         folderRepository.save(new Folder("polykhel", "Important", "yellow"));
+
+        for (int i = 0; i < 10; i++) {
+            EmailListItemKey key = new EmailListItemKey();
+            key.setId("polykhel");
+            key.setLabel("Inbox");
+            key.setTimeUUID(Uuids.timeBased());
+
+            EmailListItem item = new EmailListItem();
+            item.setKey(key);
+            item.setTo(List.of("polykhel"));
+            item.setSubject("Subject " + i);
+            item.setUnread(true);
+
+            emailListItemRepository.save(item);
+        }
     }
 }
--- src/main/java/com/polykhel/inbox/controllers/InboxController.java
+++ src/main/java/com/polykhel/inbox/controllers/InboxController.java
@@ -1,8 +1,12 @@
 package com.polykhel.inbox.controllers;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import com.polykhel.inbox.emallist.EmailListItem;
+import com.polykhel.inbox.emallist.EmailListItemRepository;
 import com.polykhel.inbox.folders.Folder;
 import com.polykhel.inbox.folders.FolderRepository;
 import com.polykhel.inbox.folders.FolderService;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -10,17 +14,22 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
 public class InboxController {
 
     private final FolderRepository folderRepository;
     private final FolderService folderService;
+    private final EmailListItemRepository emailListItemRepository;
 
-    public InboxController(FolderRepository folderRepository, FolderService folderService) {
+    public InboxController(FolderRepository folderRepository, FolderService folderService, EmailListItemRepository emailListItemRepository) {
         this.folderRepository = folderRepository;
         this.folderService = folderService;
+        this.emailListItemRepository = emailListItemRepository;
     }
 
     @GetMapping("/")
@@ -30,11 +39,22 @@ public class InboxController {
             return "index";
         }
 
+        // Fetch folders
         String userId = principal.getAttribute("login");
         List<Folder> userFolders = folderRepository.findAllById(userId);
         model.addAttribute("userFolders", userFolders);
         List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
         model.addAttribute("defaultFolders", userFolders);
+
+        // Fetch messages
+        String folderLabel = "Inbox";
+        List<EmailListItem> emailList = emailListItemRepository.findAllByKey_IdAndKey_Label(userId, folderLabel);
+        PrettyTime p = new PrettyTime();
+        model.addAttribute("emailList", emailList.stream().peek(emailItem -> {
+            UUID timeUuid = emailItem.getKey().getTimeUUID();
+            Date emailDateTime = new Date(Uuids.unixTimestamp(timeUuid));
+            emailItem.setAgoTimeString(p.format(emailDateTime));
+        }).collect(Collectors.toList()));
 
         return "inbox-page";
     }
