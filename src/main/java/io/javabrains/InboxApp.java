91fb4c59b6031d8bd94e39db4d7406915d245116
Michael Pollente <pollentem@gmail.com>
2022-01-29 22:37:14 +0800

    Add Astra db connection infrastructure

--- src/main/java/io/javabrains/InboxApp.java
+++ src/main/java/io/javabrains/InboxApp.java
@@ -2,24 +2,31 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
 public class InboxApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InboxApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InboxApp.class, args);
+    }
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
-	}
-	
+    /**
+     * This is necessary to have the Spring Boot app use the Astra secure bundle
+     * to connect to the database
+     *
+     * @param properties DataStaxAstraProperties
+     * @return CqlSessionBuilderCustomizer
+     */
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties properties) {
+        Path bundle = properties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 
 }
